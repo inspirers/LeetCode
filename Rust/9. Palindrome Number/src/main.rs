78b38f7090245d6a2074c4121f186d8ccbc8a0d1
inspirers <arvid.boisen@protonmail.com>
2022-04-03 14:10:07 +0200

    Added enter value

--- Rust/9. Palindrome Number/src/main.rs
+++ Rust/9. Palindrome Number/src/main.rs
@@ -1,28 +1,31 @@
 use std::io;
+use std::io::Write;
 
 pub fn is_palindrome(x: i32) -> bool {
-        if x < 0 {
-            return false;
-        }
-        let mut y = x;
-        let mut z = 0;
-        while y > 0 {
-                z *= 10;
-                z += y % 10;
-                y /= 10;
-                    }
-        z == x
+    if x < 0 {
+        return false;
+    }
+    let mut y = x;
+    let mut z = 0;
+    while y > 0 {
+        z *= 10;
+        z += y % 10;
+        y /= 10;
+    }
+    z == x
 }
 
-fn main(){
+fn main() {
+    print!("Enter value: ");
+    io::stdout().flush().unwrap();
     let mut input_line = String::new();
     io::stdin() // the rough equivalent of `std::cin`
-    .read_line(&mut input_line) // actually read the line
-    .expect("Failed to read line"); // which can fail, however
-let x: i32 = input_line
-    .trim() // ignore whitespace around input
-    .parse() // convert to integers
-    .expect("Input not an integer"); // which, again, can fail
+        .read_line(&mut input_line) // actually read the line
+        .expect("Failed to read line"); // which can fail, however
+    let x: i32 = input_line
+        .trim() // ignore whitespace around input
+        .parse() // convert to integers
+        .expect("Input not an integer"); // which, again, can fail
 
     println!("{}", is_palindrome(x));
 }
